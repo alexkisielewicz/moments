00d14abfcd1f59731f0ac6f981e53158cba7da4d
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-03 12:54:04 +0000

    Add req and res interceptors to refresh current user token

--- src/App.js
+++ src/App.js
@@ -6,33 +6,9 @@ import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
-import { createContext, useEffect, useState } from 'react';
-import axios from 'axios';
-
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null)
-
-  const handleMount = async () => {
-    try {
-      const { data } = await axios.get('dj-rest-auth/user/')
-      setCurrentUser(data)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-    handleMount()
-  }, [])
-
   return (
-    // this provides current user value and function to be available for every component inside it
-    // in the prop we pass the value that we want the children to have access to
-    <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
         <div className={styles.App}>
           <NavBar />
           <Container className={styles.Main}>
@@ -44,8 +20,6 @@ function App() {
             </Switch>
           </Container>
         </div>
-      </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
   );
 }
 
