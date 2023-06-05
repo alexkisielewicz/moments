54002e3eea33af4a2c4e985a38e10436076f7acf
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-05 18:31:39 +0000

    follow the lessons

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,22 +1,52 @@
-import { axiosReq } from "../api/axiosDefaults"
+import { axiosReq } from "../api/axiosDefaults";
 
-// can be used with any paginated data like posts/comments/profiles etc.
 export const fetchMoreData = async (resource, setResource) => {
-    try {
-        const {data} = await axiosReq.get(resource.next)
-        setResource(prevResource => ({
-            ...prevResource, 
-            next: data.next,
-            // we use reduce method to look through the new page of resaults from API
-            results: data.results.reduce((acc, cur) => {
-                // some method to check for duplicates, it stops as soon as it finds one common item
-                return acc.some(accResult => accResult.id === cur.id) 
-                ? acc 
-                : [...acc, cur]
-                // we append new results to previous array with resoults
-            }, prevResource.results)
-        }))
-    } catch (error) {
-        
-    }
-}
+  try {
+    const { data } = await axiosReq.get(resource.next);
+    setResource((prevResource) => ({
+      ...prevResource,
+      next: data.next,
+      results: data.results.reduce((acc, cur) => {
+        return acc.some((accResult) => accResult.id === cur.id)
+          ? acc
+          : [...acc, cur];
+      }, prevResource.results),
+    }));
+  } catch (err) {}
+};
+
+export const followHelper = (profile, clickedProfile, following_id) => {
+  return profile.id === clickedProfile.id
+    ? // This is the profile I clicked on,
+      // update its followers count and set its following id
+      {
+        ...profile,
+        followers_count: profile.followers_count + 1,
+        following_id,
+      }
+    : profile.is_owner
+    ? // This is the profile of the logged in user
+      // update its following count
+      { ...profile, following_count: profile.following_count + 1 }
+    : // this is not the profile the user clicked on or the profile
+      // the user owns, so just return it unchanged
+      profile;
+};
+
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? // This is the profile I clicked on,
+      // update its followers count and set its following id
+      {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? // This is the profile of the logged in user
+      // update its following count
+      { ...profile, following_count: profile.following_count - 1 }
+    : // this is not the profile the user clicked on or the profile
+      // the user owns, so just return it unchanged
+      profile;
+};
